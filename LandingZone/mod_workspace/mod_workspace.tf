2af111bbaa3513b326567a0dd8f84ecd01b0c170
udayanms <52168703+udayanms@users.noreply.github.com>
2020-08-09 03:25:09 +0000

    Update mod_workspace.tf

--- LandingZone/mod_workspace/mod_workspace.tf
+++ LandingZone/mod_workspace/mod_workspace.tf
@@ -14,8 +14,8 @@ provider tfe {
 
 # Add a user to the organization
 resource "github_repository" "git_repo" {
-  name         = local.params.git.repo_name
-  description  = local.params.git.repo_name
+  name         = local.params.git.target_repo_name
+  description  = local.params.git.target_repo_name
   provider     = github.github1
 # private = true
 # Valid templates would be Accuont Creation, Guard
