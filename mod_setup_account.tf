0a5d52dbf446aa58a402b8c2030b89796aa3279a
udayanms <52168703+udayanms@users.noreply.github.com>
2020-08-11 02:34:31 +0000

    Update mod_setup_account.tf

--- mod_setup_account.tf
+++ mod_setup_account.tf
@@ -20,24 +20,3 @@ resource "github_repository" "git_repo" {
     repository = local.params.git.bootstrap_template.repository
   }
 }
-
-resource "tfe_workspace" "ws" {
-  name         = local.params.tfe.tf_workspace_name
-  organization = local.params.tfe.tf_org
-  provider     = tfe.tfe1
-  vcs_repo {
-     identifier     = "${local.params.git.target_git_org}/${local.params.git.target_repo_name}"
-#     branch         = local.params.git.repo_name
-     oauth_token_id =  local.params.tfe.vcs_oauth_token_id
-  }
-   depends_on = [github_repository.git_repo]
-}
-
-resource "tfe_variable" "variable" {
-  key          = "my_key_name"
-  value        = "my_value_name"
-  category     = "terraform"
-  workspace_id = tfe_workspace.ws.id
-  description  = "a useful description"
-  provider     = tfe.tfe1
-}
