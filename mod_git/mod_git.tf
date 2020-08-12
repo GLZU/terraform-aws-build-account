1633ba9766cf0ba918be31199a8c36231ed3afb8
udayanms <52168703+udayanms@users.noreply.github.com>
2020-08-12 18:52:15 +0000

    Update mod_git.tf

--- mod_git/mod_git.tf
+++ mod_git/mod_git.tf
@@ -6,7 +6,7 @@ locals {
 resource "github_repository" "git_repo" {
   name         = local.params.git.target_repo_name
   description  = local.params.git.target_repo_name
-  provider     = github.github1
+#  provider     = github.github1
 # private = true
 # Valid templates would be Accuont Creation, Guard 
  template {
